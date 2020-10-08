57bfe9e9361398603146cac4c2ed8da2c457ffce
Aditya1001001 <luciferisanangel@gmail.com>
2020-10-08 14:05:30 +0530

    revamped front-end

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
-    return render_template('index.html')
+    return render_template('index.html',prediction_text="Price prediction will appear here")
 
 
 standard_to = StandardScaler()
@@ -24,11 +24,14 @@ def predict():
         Owner=int(request.form['Owner'])
         Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
         if(Fuel_Type_Petrol=='Petrol'):
-                Fuel_Type_Petrol=1
-                Fuel_Type_Diesel=0
-        else:
+            Fuel_Type_Petrol=1
+            Fuel_Type_Diesel=0
+        elif(Fuel_Type_Petrol=='Diesel'):
             Fuel_Type_Petrol=0
             Fuel_Type_Diesel=1
+        else:
+            Fuel_Type_Petrol=0
+            Fuel_Type_Diesel=0
         Year=2020-Year
         Seller_Type_Individual=request.form['Seller_Type_Individual']
         if(Seller_Type_Individual=='Individual'):
@@ -45,7 +48,7 @@ def predict():
         if output<0:
             return render_template('index.html',prediction_text="Sorry we can't help you :( ")
         else:
-            return render_template('index.html',prediction_text="The car can be sold for: {} lakhs".format(output))
+            return render_template('index.html',prediction_text="You can sell the car for roughly {} lakhs".format(output))
     else:
         return render_template('index.html')
 
